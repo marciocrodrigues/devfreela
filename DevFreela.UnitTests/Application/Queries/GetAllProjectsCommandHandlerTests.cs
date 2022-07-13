dc13858145801ef93f5ed354d4ef39957f888ff9
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2022-07-13 19:59:01 -0300

    feat: Test Implementation

---
+++ DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Queries
+{
+    public class GetAllProjectsCommandHandlerTests
+    {
+        [Fact]
+        public async Task ThreeProjectsExists_Executed_ReturnThreeProjectViewmodels()
+        {
+            // Arrange
+            var projects = new List<Project>
+            {
+                new Project("Nome Do Teste 1", "Descricao De Teste 1", 1, 2, 10000),
+                new Project("Nome Do Teste 2", "Descricao De Teste 2", 1, 2, 20000),
+                new Project("Nome Do Teste 3", "Descricao De Teste 3", 1, 2, 30000)
+            };
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            projectRepositoryMock.Setup(pr => pr.GetAll().Result).Returns(projects);
+
+            var getAllProjectQuery = new GetAllProjectsQuery("");
+            var getAllProjectsQueryHandler = new GetAllProjectsQueryHandler(projectRepositoryMock.Object);
+
+            // Act
+            var projectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectQuery, new CancellationToken());
+
+            // Assert
+            Assert.NotNull(projectViewModelList);
+            Assert.NotEmpty(projectViewModelList);
+            Assert.Equal(projects.Count, projectViewModelList.Count);
+
+            // Valida se o metodo foi chamado e quantas vezes, no caso uma vez
+            projectRepositoryMock.Verify(pr => pr.GetAll().Result, Times.Once);
+        }
+    }
+}
---
+++ DevFreela.UnitTests/Application/Queries/GetAllSkillsQueryHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using DevFreela.Application.Queries.GetAllSkills;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Queries
+{
+    public class GetAllSkillsQueryHandlerTests
+    {
+        [Fact]
+        public async Task ThreeSkillsExists_Executed_ReturnThreeSkillsViewmodels()
+        {
+            // Arrange
+            var skillsList = new List<Skill>
+            {
+                new Skill("Skill Teste 1"),
+                new Skill("Skill Teste 2"),
+                new Skill("Skill Teste 3")
+            };
+
+            var skillRepositoryMock = new Mock<ISkillRepository>();
+            skillRepositoryMock.Setup(pr => pr.GetAllSkills().Result).Returns(skillsList);
+
+            var getAllSkillsQuery = new GetAllSkillsQuery();
+            var getAllSkillsQueryHandler = new GetAllSkillsQueryHandler(skillRepositoryMock.Object);
+
+            // Act
+            var skillsViewModelList = await getAllSkillsQueryHandler.Handle(getAllSkillsQuery, new CancellationToken());
+
+            // Assert
+            Assert.NotNull(skillsViewModelList);
+            Assert.NotEmpty(skillsViewModelList);
+            Assert.Equal(skillsList.Count, skillsViewModelList.Count);
+
+            // Valida se o metodo foi chamado e quantas vezes, no caso uma vez
+            skillRepositoryMock.Verify(pr => pr.GetAllSkills().Result, Times.Once);
+        }
+    }
+}
